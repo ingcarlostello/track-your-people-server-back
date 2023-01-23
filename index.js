92346589b4e9e6d8e397e69c4ff30efd5e007cba
Carlos Tello <ingcarlostello@hotmail.com>
2023-01-22 23:05:10 -0500

    quitando los headers back

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
             origin: '*',
             methods: ["GET", "POST"],
             credentials: true,
-            allowedHeaders: ["my-custom-header"],
+
      
             
       }
