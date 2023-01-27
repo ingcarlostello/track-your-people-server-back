1bf4ea1ab470d0912782f2fe7b56511e6032589c
Carlos Tello <ingcarlostello@hotmail.com>
2023-01-26 19:04:57 -0500

    refactorizacion del servidor

--- index.js
+++ index.js
@@ -1,121 +1,38 @@
-
 import express from "express";
-//import cors from "cors";
-import { Server } from "socket.io";
-import cluster from 'cluster'
-import sticky from 'sticky-session'
-import http from 'http'
-
-
-
-
-  
+import http from "http"; // modulo de node
+import { Server as SocketServer } from "socket.io";
+import { activeMarkers, addMarker } from "./models/marcadores.js";
 
 const app = express();
-//app.use(cors());
-
+const server = http.Server(app);
+const io = new SocketServer(server, { cors: { origin: "*" } });
 const port = 4000;
 
-const server = http.Server(app);
-// var server = require('http').createServer(function(req, res) {
-//   res.end('worker: ' + cluster.worker.id);
-// });
-
 app.get("/", (req, res) => {
-  console.log('worker: ' + cluster.worker.id);
   res.send("Hello World!");
 });
 
-// const server = app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
-if (!sticky.listen(server, 4000)) {
-  // Master code
-  const io = new Server(server, {
-
-    transports: [ "polling", "websocket"  ],
-      cors: {
-        
-            // origin: 'http://127.0.0.1:5173'
-            //origin: 'https://track-your-people-front.vercel.app',
-            origin: '*',
-            methods: ["GET", "POST"],
-            credentials: true,
-
-     
-            
-      }
-});
 
 
-
-
-
-
-let onlyActiveMarkers
+let onlyActiveMarkers;
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-
-
   socket.on("new-marker", (markerPosition) => {
-
-
-
     addMarker(markerPosition);
-
-
-
-    console.log('array de marcadres activos--->', activeMarkers);
-
-
-    onlyActiveMarkers = activeMarkers.filter(item => item.markerId != '')
-
-
-
+    console.log("array de marcadres activos--->", activeMarkers);
+    onlyActiveMarkers = activeMarkers.filter((item) => item.markerId != "");
     socket.broadcast.emit("emit-new-marker", onlyActiveMarkers); // everyone gets it but the sender
   });
 
-
   socket.on("moving-marker", (movingMarker) => {
-
-    console.log('movingMarker--->', movingMarker);
-    socket.broadcast.emit('moving-marker', movingMarker);
-    
-
-  })
-
-
-
-});
-  server.once('listening', function() {
-    console.log('server started on 4000 port');
+    console.log("movingMarker--->", movingMarker);
+    socket.broadcast.emit("moving-marker", movingMarker);
   });
-} 
+});
+
+server.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 
 
 
-
-
-
-
-
-
-
-
-
-// if(!sticky.listen(server,port))
-// {
-//   server.once('listening', function() {
-//     console.log('Server started on port '+port);
-//   });
-
-//   if (cluster.isMaster) {
-//     console.log('Master server started on port '+port);
-//   } 
-// }
-// else {
-//     console.log('- Child server started on port '+port+' case worker id='+cluster.worker.id);
-//   }
