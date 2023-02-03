a69ef0551f884bfbcbb432483d63e5c1916a1db3
Carlos Tello <ingcarlostello@hotmail.com>
2023-02-02 22:47:16 -0500

    probando sticky session

--- index.js
+++ index.js
@@ -1,137 +1,112 @@
-import cluster from 'cluster'
+import cluster from "cluster";
 import express from "express";
 import http from "http"; // modulo de node
 import { Server as SocketServer } from "socket.io";
 import { activeMarkers, addMarker } from "./models/marcadores.js";
-import numCPUs from 'os'
-import {setupMaster, setupWorker} from '@socket.io/sticky'
-import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter'
 
+import { cpus } from "os";
+import { setupMaster, setupWorker } from "@socket.io/sticky";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
+const numCPUs = cpus().length;
 
-
-const cpu = numCPUs.cpus().length
-
-
-
+const port = 4000;
 const app = express();
-const server = http.Server(app);
 
-if (cluster.isMaster) {
+
+if (cluster.isPrimary) {
   console.log(`Master ${process.pid} is running`);
+  const server = http.Server(app);
 
-  const httpServer = http.createServer();
-
-  setupMaster(httpServer, {
-    loadBalancingMethod: "least-connection", // either "random", "round-robin" or "least-connection"
+  // setup sticky sessions
+  setupMaster(server, {
+    loadBalancingMethod: "least-connection",
   });
 
+  // setup connections between the workers
   setupPrimary();
 
-  httpServer.listen(3000);
+  cluster.setupMaster({
+    serialization: "advanced",
+  });
 
-  for (let i = 0; i < cpu; i++) {
+  server.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+  for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
   cluster.on("exit", (worker) => {
     console.log(`Worker ${worker.process.pid} died`);
     cluster.fork();
   });
-} else {
+}else {
+
   console.log(`Worker ${process.pid} started`);
 
+  const server = http.Server(app);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "https://track-your-people-front.vercel.app");
-    next();
-  });
-  
   const io = new SocketServer(server, {
-    path: '/socket.io',
-    
     cors: {
-      origin: ["https://track-your-people-front.vercel.app"],
+      //origin: "https://track-your-people-front.vercel.app",
+      origin: "*",
       credentials: true,
       methods: ["GET", "POST"],
-      allowedHeaders:['my-custom-header', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
     },
   });
-  
-  
-  
-  const port = 4000;
-  
+
+  // use the cluster adapter
+  io.adapter(createAdapter());
+
+  // setup connection with the primary process
+  setupWorker(io);
+
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
-  
-  
-  
+
   let onlyActiveMarkers;
-
-
- 
-  io.adapter(createAdapter());
-  setupWorker(io);
-
-
-    /* ... */
-  
-    
-    io.on("connection", (socket) => {
-      console.log("a user connected");
-      console.log('socket', socket.id);
-      socket.on("new-marker", (markerPosition) => {
-        addMarker(markerPosition);
-        console.log("array de marcadres activos--->", activeMarkers);
-        onlyActiveMarkers = activeMarkers.filter((item) => item.markerId != "");
-        socket.broadcast.emit("emit-new-marker", onlyActiveMarkers); // everyone gets it but the sender
-      });
-    
-      socket.on("moving-marker", (movingMarker) => {
-        console.log("movingMarker--->", movingMarker);
-        socket.broadcast.emit("moving-marker", movingMarker);
-      });
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    console.log("socket", socket.id);
+    socket.on("new-marker", (markerPosition) => {
+      addMarker(markerPosition);
+      console.log("array de marcadres activos--->", activeMarkers);
+      onlyActiveMarkers = activeMarkers.filter((item) => item.markerId != "");
+      socket.broadcast.emit("emit-new-marker", onlyActiveMarkers); // everyone gets it but the sender
     });
-    
-    server.listen(port, () => console.log(`Example app listening on port ${port}`));
-    
+  
+    socket.on("moving-marker", (movingMarker) => {
+      console.log("movingMarker--->", movingMarker);
+      socket.broadcast.emit("moving-marker", movingMarker);
+    });
+  });
+
 
 }
 
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "https://track-your-people-front.vercel.app");
-//   next();
-// });
+// const app = express();
+//const server = http.Server(app);
 
 // const io = new SocketServer(server, {
-//   path: '/socket.io',
-  
 //   cors: {
-//     origin: ["https://track-your-people-front.vercel.app"],
+//     //origin: "https://track-your-people-front.vercel.app",
+//     origin: "*",
 //     credentials: true,
 //     methods: ["GET", "POST"],
-//     allowedHeaders:['my-custom-header', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
 //   },
 // });
 
-
-
-// const port = 4000;
+//const port = 4000;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
 // });
 
-
-
 // let onlyActiveMarkers;
 
 // io.on("connection", (socket) => {
 //   console.log("a user connected");
-//   console.log('socket', socket.id);
+//   console.log("socket", socket.id);
 //   socket.on("new-marker", (markerPosition) => {
 //     addMarker(markerPosition);
 //     console.log("array de marcadres activos--->", activeMarkers);
@@ -145,8 +120,4 @@ if (cluster.isMaster) {
 //   });
 // });
 
-// server.listen(port, () => console.log(`Example app listening on port ${port}`));
-
-
-
-
+//server.listen(port, () => console.log(`Example app listening on port ${port}`));
